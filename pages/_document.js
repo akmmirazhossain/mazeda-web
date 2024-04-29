2e9d5dee385b33ba15376e7510ce69b518ac3956
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2024-04-29 19:06:28 +0600

    SEO Works

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GM0K28GTRH"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-GM0K28GTRH');
+              `,
+          }}
+        />
+        {/* End Google Analytics */}
+      </Head>
       <body>
         <Main />
         <NextScript />
