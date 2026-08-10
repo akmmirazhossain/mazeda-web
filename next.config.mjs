07dd4a560db87b91df3c499fd419a597763c82b9
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2026-08-10 20:37:33 +0600

    https redirect

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,19 @@ const nextConfig = {
     defaultLocale: "en",
     localeDetection: false,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          { type: "host", value: "www.mazeda.net" },
+          { type: "header", key: "x-forwarded-proto", value: "http" },
+        ],
+        destination: "https://www.mazeda.net/:path*",
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
